bcea98e21e172e54e97a06e659d50d2e3391a301
Cenk Altı <cenkalti@gmail.com>
2025-04-08 12:36:36 -0400

    update run script

--- run.sh
+++ run.sh
@@ -1,1 +1,3 @@
-ALLOW_ORIGINS="http://localhost:5173" uv run fastapi dev --host 0.0.0.0 --port 8000 --reload
+#!/bin/sh
+export ALLOW_ORIGINS="http://localhost:5173"
+exec uv run fastapi dev --host 0.0.0.0 --port 8000 --reload
