088e8c106aa248bf824b464e7b9ee4c2390697f4
Cenk Altı <cenkalti@gmail.com>
2025-04-17 19:51:13 -0400

    tmp: litellm

--- framework.py
+++ framework.py
@@ -1,26 +1,16 @@
 import os
-import re
 import time
 from datetime import datetime
+
+# from io import StringIO  # TODO use StringIO to accumulate message content and function arguments
 from typing import Any, Optional, Sequence
 
-from openai import AsyncOpenAI
-from openai.lib.streaming.chat import (
-    ChunkEvent,
-    ContentDeltaEvent,
-    FunctionToolCallArgumentsDeltaEvent,
-    RefusalDeltaEvent,
-)
-from openai.types.chat import (
-    ChatCompletionAssistantMessageParam,
-    ChatCompletionMessageParam,
+from litellm import CustomStreamWrapper, acompletion
+from litellm.types.utils import (
     ChatCompletionMessageToolCall,
-    ChatCompletionMessageToolCallParam,
-    ChatCompletionSystemMessageParam,
-    ChatCompletionUserMessageParam,
-    ParsedChatCompletionMessage,
+    Function,
+    Message,
 )
-from openai.types.chat.chat_completion_message_tool_call_param import Function
 from pydantic import BaseModel
 
 from akson import Assistant, Chat, ChatState
@@ -45,7 +35,6 @@ class SimpleAssistant(Assistant):
         self.max_turns = max_turns
         self.examples: list[tuple[str, BaseModel]] = []
         self._name = name
-        self._client = AsyncOpenAI()
 
     @property
     def name(self) -> str:
@@ -55,14 +44,13 @@ class SimpleAssistant(Assistant):
         state = ChatState(id="temp", messages=[])
         # TODO generate id automatically
         # TODO generate name automatically (is name really required?)
-        state.messages.append(
-            {"id": "asldkfjaslkfj", "role": "assistant", "name": "asldkjlkfaj", "content": user_message}
-        )
+        state.messages.append({"id": "asldkfjaslkfj", "role": "user", "name": "asldkjlkfaj", "content": user_message})
         chat = Chat(state)
         # TODO why setting _assistant needed before run() ?
         chat._assistant = self
         await self.run(chat)
         if self.output_type:
+            print(f"Structured output: {chat._structured_output}")
             assert isinstance(chat._structured_output, self.output_type)
             return chat._structured_output
 
@@ -76,7 +64,7 @@ class SimpleAssistant(Assistant):
         messages = self._get_openai_messages(chat)
 
         message = await self._complete(messages, chat)
-        messages.append(_convert_assistant_message(message))
+        messages.append(message)
 
         # We keep continue hitting OpenAI API until there are no more tool calls.
         current_turn = 0
@@ -90,31 +78,37 @@ class SimpleAssistant(Assistant):
                 messages.extend(tool_calls)
 
                 message = await self._complete(messages, chat)
-                messages.append(_convert_assistant_message(message))
+                messages.append(message)
 
-    def _get_openai_messages(self, chat: Chat) -> list[ChatCompletionMessageParam]:
-        messages: Sequence[ChatCompletionMessageParam] = []
-        messages.append(ChatCompletionSystemMessageParam(role="system", content=self._get_system_prompt()))
-        for user_message, response in self.examples:
-            messages.extend(
-                [
-                    {"role": "system", "name": "example_user", "content": user_message},
-                    {"role": "system", "name": "example_assistant", "content": response.model_dump_json()},
-                ]
-            )
+    def _get_openai_messages(self, chat: Chat) -> list[Message]:
+        messages: Sequence[Message] = []
+
+        # Add system prompt
+        msg = Message(content=self._get_system_prompt())
+        msg.role = "system"
+        messages.append(msg)
+
+        # TODO add examples
+        # for user_message, response in self.examples:
+        #     messages.extend(
+        #         [
+        #             {"role": "system", "name": "example_user", "content": user_message},
+        #             {"role": "system", "name": "example_assistant", "content": response.model_dump_json()},
+        #         ]
+        #     )
+
+        # Add user and assistant messages
         for message in chat.state.messages:
             if message.get("category"):
                 continue
             if message["role"] == "user":
-                messages.append(
-                    ChatCompletionUserMessageParam(role="user", name=message["name"], content=message["content"])
-                )
+                msg = Message(content=message["content"], provider_specific_fields={"name": message["name"]})
+                msg.role = "user"
+                messages.append(msg)
             elif message["role"] == "assistant":
-                messages.append(
-                    ChatCompletionAssistantMessageParam(
-                        role="assistant", name=message["name"], content=message["content"]
-                    )
-                )
+                msg = Message(content=message["content"], provider_specific_fields={"name": message["name"]})
+                msg.role = "assistant"
+                messages.append(msg)
         return messages
 
     def _get_system_prompt(self) -> str:
@@ -122,15 +116,17 @@ class SimpleAssistant(Assistant):
         o = time.strftime("%z")  # Timezone offset
         return f"{self.system_prompt}\n\nToday's date and time is {t} ({o})."
 
-    async def _complete(self, messages: list[ChatCompletionMessageParam], chat: Chat) -> ParsedChatCompletionMessage:
+    async def _complete(self, messages: list[Message], chat: Chat) -> Message:
         logger.info("Completing chat")
         for message in messages:
-            if "name" in message:
-                message["name"] = re.sub(r"[^a-zA-Z0-9_-]", "_", message["name"])
+            # TODO handle assistant names
+            # if "name" in message:
+            #     message["name"] = re.sub(r"[^a-zA-Z0-9_-]", "_", message["name"])
             logger.debug(message)
 
+        # TODO handle streaming of message; track state (which phase are we in?)
         # Because we're streaming, we need to track whether a message has started or not.
-        message_started = False
+        # message_started = False
 
         kwargs = {}
         if self.toolkit:
@@ -140,43 +136,67 @@ class SimpleAssistant(Assistant):
                 kwargs["tool_choice"] = "auto"
                 kwargs["parallel_tool_calls"] = False
 
+        print(f"tools: {kwargs.get('tools')}")
+        print(f"tool_choice: {kwargs.get('tool_choice')}")
+
         if self.output_type:
             kwargs["response_format"] = self.output_type
 
-        async with self._client.beta.chat.completions.stream(
+        response = await acompletion(
             model=os.environ["OPENAI_MODEL"],
             messages=messages,
+            stream=True,
             **kwargs,
-        ) as stream:
-            async for event in stream:
-                match event:  # https://github.com/openai/openai-python/blob/main/helpers.md#chat-completions-events
-                    case ContentDeltaEvent():
-                        if not message_started:
-                            message_started = True
-                            await chat.begin_message()
-                        await chat.add_chunk(event.delta)
-                    case RefusalDeltaEvent():
-                        if not message_started:
-                            message_started = True
-                            await chat.begin_message()
-                        await chat.add_chunk(event.delta)
-                    case FunctionToolCallArgumentsDeltaEvent():
-                        if not message_started:
-                            message_started = True
-                            await chat.begin_message(category="info")
-                            await chat.add_chunk(f"Calling function: {event.name}(")
-                        await chat.add_chunk(event.arguments_delta)
-                    case ChunkEvent() if event.chunk.choices[0].finish_reason:
-                        choice = event.snapshot.choices[0]
-                        if choice.finish_reason == "tool_calls":
-                            await chat.add_chunk(")")
-                        await chat.end_message()
-                        if choice.finish_reason not in ("stop", "tool_calls"):
-                            raise NotImplementedError(f"finish_reason={choice.finish_reason}")
-                        if self.output_type:
-                            assert isinstance(choice.message.parsed, self.output_type)
-                            await chat.set_structured_output(choice.message.parsed)
-                        return choice.message
+        )
+        print(response)
+        assert isinstance(response, CustomStreamWrapper)
+        await chat.begin_message()
+
+        # We will aggregate delta messages and store them in this variable until we see a finish_reason.
+        # This is the only way to get the full content of the message.
+        # We'll return this value at the end of the function.
+        message = Message(role="assistant", content="")
+        function = Function(name="", arguments="")
+        tool_call = ChatCompletionMessageToolCall(function=function)
+
+        async for chunk in response:
+            assert chunk.__class__.__name__ == "ModelResponseStream"
+            print(chunk)
+            assert len(chunk.choices) == 1
+            choice = chunk.choices[0]
+            if choice.delta.tool_calls:
+                if not message.tool_calls:
+                    message.tool_calls = [tool_call]
+                if choice.delta.tool_calls[0].function.name:
+                    assert isinstance(function.name, str)
+                    function.name += choice.delta.tool_calls[0].function.name
+                    await chat.add_chunk(f"Calling function: {function.name}(")
+                if choice.delta.tool_calls[0].function.arguments:
+                    function.arguments += choice.delta.tool_calls[0].function.arguments
+                    await chat.add_chunk(choice.delta.tool_calls[0].function.arguments)
+            if choice.delta.role:
+                message.role = choice.delta.role
+            if choice.delta.content:
+                message.content += choice.delta.content
+                await chat.add_chunk(choice.delta.content)
+
+            finish_reason = choice.finish_reason
+            if finish_reason:
+                if finish_reason == "stop":
+                    if self.output_type:
+                        assert isinstance(message.content, str)
+                        instance = self.output_type.model_validate_json(message.content)
+                        await chat.set_structured_output(instance)
+                elif finish_reason == "tool_calls":
+                    assert isinstance(message.content, str)
+                    await chat.add_chunk(")")
+                else:
+                    raise NotImplementedError(f"finish_reason={finish_reason}")
+
+                await chat.end_message()
+                return message
+
+            # await chat.begin_message(category="info")
 
         raise Exception("Stream ended unexpectedly")
 
@@ -191,25 +211,6 @@ class SimpleAssistant(Assistant):
     def add_example(self, user_message: str, response: BaseModel):
         """Add an example to the prompt."""
         self.examples.append((user_message, response))
-
-
-def _convert_assistant_message(message: ParsedChatCompletionMessage) -> ChatCompletionAssistantMessageParam:
-    if message.tool_calls:
-        tool_calls = list(map(_convert_tool_call, message.tool_calls))
-        return ChatCompletionAssistantMessageParam(role="assistant", content=message.content, tool_calls=tool_calls)
-    else:
-        return ChatCompletionAssistantMessageParam(role="assistant", content=message.content)
-
-
-def _convert_tool_call(tool_call: ChatCompletionMessageToolCall) -> ChatCompletionMessageToolCallParam:
-    return ChatCompletionMessageToolCallParam(
-        id=tool_call.id,
-        function=Function(
-            name=tool_call.function.name,
-            arguments=tool_call.function.arguments,
-        ),
-        type=tool_call.type,
-    )
 
 
 class DeclarativeAssistant(SimpleAssistant):
