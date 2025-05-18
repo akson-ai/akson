3935086bb4514e1b24e871a2ce2da568219dcf67
Cenk Altı <cenkalti@gmail.com>
2025-05-18 01:23:34 -0400

    customize log level

--- api/loader.py
+++ api/loader.py
@@ -16,18 +16,19 @@ T = TypeVar("T")
 def load_objects(object_type: type[T], dirname: str) -> dict[str, T]:
     objects = {}
     objects_dir = os.path.join(os.path.dirname(__file__), dirname)
-    logger.info("Loading objects from directory: %s", objects_dir)
+    logger.info("Loading %s objects from directory: %s", object_type.__name__, objects_dir)
     for object_file in os.listdir(objects_dir):
         object_file = os.path.basename(object_file)
         module_name, extension = os.path.splitext(object_file)
         if extension != ".py":
             continue
-        logger.info("Loading file: %s", object_file)
-        module_name = f"{dirname}.{module_name}"
-        module = importlib.import_module(module_name)
+        logger.debug("Loading file: %s", object_file)
+        module_path = f"{dirname}.{module_name}"
+        module = importlib.import_module(module_path)
         for key, value in vars(module).items():
             if not isinstance(value, object_type):
                 continue
-            objects[key] = value
+            object_id = f"{module_name}.{key}"
+            objects[object_id] = value
             logger.info("Object loaded: %s", key)
     return objects
--- api/logger.py
+++ api/logger.py
@@ -1,8 +1,14 @@
 import logging
+import os
 import sys
 
+from dotenv import load_dotenv
 from rich.console import Console
 from rich.logging import RichHandler
+
+load_dotenv()
+
+LEVEL = os.getenv("LOG_LEVEL", "INFO")
 
 FORMAT = "%(message)s"
 
@@ -10,5 +16,5 @@ handler = RichHandler(console=Console(file=sys.stderr))
 handler.setFormatter(logging.Formatter(FORMAT, datefmt="[%X]"))
 
 logger = logging.getLogger("rich")
-logger.setLevel(logging.DEBUG)
+logger.setLevel(logging.getLevelNamesMapping()[LEVEL])
 logger.addHandler(handler)
