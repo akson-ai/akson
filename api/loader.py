2172434aaa6f039bd1c23ed49b570b1533de6c6b
Cenk Altı <cenkalti@gmail.com>
2025-05-19 01:38:48 -0400

    rename

--- api/loader.py
+++ api/loader.py
@@ -5,9 +5,9 @@ from typing import Iterator
 from logger import logger
 
 
-def load_objects[T](object_type: type[T], dirname: str) -> Iterator[T]:
+def load_objects[T](ObjectType: type[T], dirname: str) -> Iterator[T]:
     objects_dir = os.path.join(os.path.dirname(__file__), dirname)
-    logger.info("Loading %s objects from directory: %s", object_type.__name__, objects_dir)
+    logger.info("Loading %s objects from directory: %s", ObjectType.__name__, objects_dir)
     for object_file in os.listdir(objects_dir):
         object_file = os.path.basename(object_file)
         module_name, extension = os.path.splitext(object_file)
@@ -17,7 +17,7 @@ def load_objects[T](object_type: type[T], dirname: str) -> Iterator[T]:
         module_path = f"{dirname}.{module_name}"
         module = importlib.import_module(module_path)
         for key, value in vars(module).items():
-            if not isinstance(value, object_type):
+            if not isinstance(value, ObjectType):
                 continue
             logger.info("Loaded object: %s", key)
             yield value
