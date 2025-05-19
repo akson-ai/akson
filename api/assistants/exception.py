5add2919032e43a9d130e1721962e3592adf3ed0
Cenk Altı <cenkalti@gmail.com>
2025-05-19 01:39:15 -0400

    fix warning

--- api/assistants/exception.py
+++ api/assistants/exception.py
@@ -4,7 +4,7 @@ from akson import Assistant, Chat
 class ExceptionRaiser(Assistant):
 
     async def run(self, chat: Chat) -> None:
-        1 / 0
+        1 / 0  # type: ignore
 
 
 exception_raiser = ExceptionRaiser()
