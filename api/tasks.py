b37c79d2c0b96783cfda432fd6f006a1adfa3fa9
Cenk Altı <cenkalti@gmail.com>
2025-05-20 17:06:20 -0400

    rename

--- api/tasks.py
+++ api/tasks.py
@@ -4,8 +4,8 @@ from akson import Chat
 from framework import Agent
 
 
-async def update_title(self: Chat):
-    if self.state.title:
+async def update_title(chat: Chat):
+    if chat.state.title:
         return
 
     class TitleResponse(BaseModel):
@@ -17,12 +17,12 @@ async def update_title(self: Chat):
         Output a title for the conversation.
     """
     input = (
-        f"<user>{self.state.messages[0].content}</user>\n\n" f"<assistant>{self.state.messages[1].content}</assistant>"
+        f"<user>{chat.state.messages[0].content}</user>\n\n" f"<assistant>{chat.state.messages[1].content}</assistant>"
     )
     titler = Agent(name="Titler", model="gpt-4.1-nano", system_prompt=instructions, output_type=TitleResponse)
     response = await titler.respond(input)
     assert isinstance(response, TitleResponse)
-    self.state.title = response.title
-    await self._queue_message({"type": "update_title", "title": self.state.title})
+    chat.state.title = response.title
+    await chat._queue_message({"type": "update_title", "title": chat.state.title})
     # TODO Fix race condition
-    self.state.save_to_disk()
+    chat.state.save_to_disk()
