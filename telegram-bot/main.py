aa534c77841edb8773f64286efb2b9f82c7f32f2
Cenk Altı <cenkalti@gmail.com>
2025-05-21 10:07:09 -0400

    listen replies from sse

--- telegram-bot/main.py
+++ telegram-bot/main.py
@@ -3,12 +3,14 @@ import logging
 import os
 import uuid
 from functools import wraps
+from io import StringIO
 
 from akson_client import AksonClient
 from dotenv import load_dotenv
 from telegram import Update
-from telegram.constants import ChatAction
+from telegram.constants import ChatAction, ParseMode
 from telegram.ext import (
+    Application,
     ApplicationBuilder,
     CommandHandler,
     ContextTypes,
@@ -26,12 +28,58 @@ AKSON_API_BASE_URL = os.environ.get("AKSON_API_BASE_URL", "http://localhost:8000
 logging.basicConfig(format="%(asctime)s - %(name)s - %(levelname)s - %(message)s", level=logging.INFO)
 logger = logging.getLogger(__name__)
 
-client = AksonClient(AKSON_API_BASE_URL)
+akson = AksonClient(AKSON_API_BASE_URL)
+listener: "Listener"
 
-# Global state
-# TODO save state in telegram chat context
-akson_chat_id = str(uuid.uuid4()).replace("-", "")
-event_listener_task: asyncio.Task
+
+async def post_init(app: Application):
+    global listener
+    listener = Listener(app)
+
+
+class Listener:
+
+    def __init__(self, app: Application):
+        self.app = app
+        # TODO load state and set both akson_chat_id and telegram_chat_id
+        self.akson_chat_id = str(uuid.uuid4()).replace("-", "")
+        self.telegram_chat_id: int | None = None
+        self._task: asyncio.Task | None = None
+        self._start()
+
+    async def set_akson_chat_id(self, akson_chat_id: str):
+        if akson_chat_id != self.akson_chat_id:
+            self.akson_chat_id = akson_chat_id
+            self._start()
+
+    async def set_telegram_chat_id(self, telegram_chat_id: int):
+        self.telegram_chat_id = telegram_chat_id
+
+    def _start(self):
+        if self._task:
+            self._task.cancel()
+        self._task = self.app.create_task(self._listen_events(self.akson_chat_id))
+
+    async def _listen_events(self, akson_chat_id: str):
+        logger.info("Listening for events...")
+        content = StringIO()
+        async for event in akson.stream_events(akson_chat_id):
+            logger.info(f"Event: {event}")
+            match event["type"]:
+                case "begin_message":
+                    content.truncate(0)
+                    if self.telegram_chat_id:
+                        await self.app.bot.send_chat_action(chat_id=self.telegram_chat_id, action=ChatAction.TYPING)
+                case "add_chunk":
+                    if event["location"] == "content":
+                        content.write(event["chunk"])
+                case "end_message":
+                    if self.telegram_chat_id:
+                        text = markdownify(content.getvalue())
+                        await self.app.bot.send_message(
+                            chat_id=self.telegram_chat_id, text=text, parse_mode=ParseMode.MARKDOWN_V2
+                        )
+                    content.truncate(0)
 
 
 def restricted(func):
@@ -46,22 +94,12 @@ def restricted(func):
     return wrapped
 
 
-async def listen_events(chat_id):
-    logger.info("Listening for events...")
-    async for event in client.stream_events(chat_id):
-        logger.info(f"Event: {event}")
-
-
 @restricted
-async def handle_new(update: Update, context: ContextTypes.DEFAULT_TYPE):
+async def handle_new(update: Update, _: ContextTypes.DEFAULT_TYPE):
     assert update.message
     logger.info("handle_new")
-    global akson_chat_id
-    global event_listener_task
-    akson_chat_id = str(uuid.uuid4()).replace("-", "")
-    await update.message.reply_text(text=f"New chat created: {akson_chat_id}")
-    event_listener_task.cancel()
-    event_listener_task = context.application.create_task(listen_events(akson_chat_id))
+    await listener.set_akson_chat_id(str(uuid.uuid4()).replace("-", ""))
+    await update.message.reply_text(text=f"New chat created: {listener.akson_chat_id}")
 
 
 @restricted
@@ -70,16 +108,10 @@ async def handle_message(update: Update, context: ContextTypes.DEFAULT_TYPE):
     assert update.message
     assert update.message.text
     logger.info("handle_message")
+    await listener.set_telegram_chat_id(update.effective_chat.id)
+    # TODO move typing under "add_chunk" event
     await context.bot.send_chat_action(chat_id=update.effective_chat.id, action=ChatAction.TYPING)
-    replies = await client.send_message(akson_chat_id, update.message.text)
-    for reply in replies:
-        text = markdownify(reply["content"])
-        await update.message.reply_markdown_v2(text=text)
-
-
-async def post_init(app):
-    global event_listener_task
-    event_listener_task = app.create_task(listen_events(akson_chat_id))
+    await akson.send_message(listener.akson_chat_id, update.message.text)
 
 
 application = ApplicationBuilder().token(TELEGRAM_TOKEN).build()
