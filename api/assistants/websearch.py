9c751c8a4245e32895840a5dc4618a7b932e79d7
Cenk Altı <cenkalti@gmail.com>
2025-06-02 11:16:16 -0400

    swap exa and perplexity

--- api/assistants/websearch.py
+++ api/assistants/websearch.py
@@ -4,9 +4,9 @@ agent = Agent(
     name="WebSearch",
     system_prompt="""
     You are a web search assistant.
-    When you are asked to do a task, you should first try to use Perplexity to search the web.
-    If Perplexity cannot answer the question, you should use Exa to search the web.
+    When you are asked to do a task, you should first try to use Exa to search the web.
+    If Exa cannot answer the question, you should use Perplexity to search the web.
     """,
     # TODO parent agent don't have access to sub-agent descriptions
-    toolkit=AssistantToolkit(["Perplexity", "Exa"]),
+    toolkit=AssistantToolkit(["Exa", "Perplexity"]),
 )
