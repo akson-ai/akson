5ec916b3867a2a69c68d14877a3155aa86c63028
Cenk Altı <cenkalti@gmail.com>
2025-06-03 14:21:55 -0400

    use docker mcp in gmail

--- api/assistants/gmail.py
+++ api/assistants/gmail.py
@@ -1,14 +1,41 @@
+import os
+
 from framework import Agent, MCPToolkit
+
+GMAIL_KEYS_DIR = os.environ["GMAIL_KEYS_DIR"]
+NPM_CACHE_DIR = os.environ["NPM_CACHE_DIR"]
+
+cmd = [
+    "docker",
+    "run",
+    "-i",
+    "--rm",
+    "--entrypoint",
+    "/usr/local/bin/npm",
+    "--mount",
+    f"type=bind,source={GMAIL_KEYS_DIR},target=/keys",
+    "--mount",
+    f"type=bind,source={NPM_CACHE_DIR},target=/root/.npm",
+    "-v",
+    "mcp-gmail:/gmail-server",
+    "-e",
+    "GMAIL_OAUTH_PATH=/keys/gcp-oauth.keys.json",
+    "-e",
+    "GMAIL_CREDENTIALS_PATH=/keys/credentials.json",
+    "-p",
+    "3000:3000",
+    "node:24",
+    "exec",
+    "@gongrzhe/server-gmail-autoauth-mcp",
+]
 
 gmail = Agent(
     name="Gmail",
     system_prompt=(
         "You are Gmail assistant."
-        "Try bringing 100 results when searching for emails."
+        "Try bringing at least 100 results when searching for emails."
         "Get confirmation before performing any actions that modify data."
+        f"When access token is not valid, instruct user to authenticate by running `{' '.join(cmd)} auth` command."
     ),
-    toolkit=MCPToolkit(
-        command="npx",
-        args=["-y", "@gongrzhe/server-gmail-autoauth-mcp"],
-    ),
+    toolkit=MCPToolkit(command=cmd[0], args=cmd[1:]),
 )
