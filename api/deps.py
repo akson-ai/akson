354ecbb6e97071e07a9cf6d9ca848b457eb9d71c
Cenk Altı <cenkalti@gmail.com>
2025-06-05 22:37:45 -0400

    make sure default assistant exists

--- api/deps.py
+++ api/deps.py
@@ -5,10 +5,10 @@ from fastapi import Depends
 import models
 from akson import Assistant, Chat, ChatState
 from pubsub import PubSub
-from registry import Registry
+from registry import Registry, UnknownAssistant
 
 # Load environment variables
-default_assistant = os.getenv("DEFAULT_ASSISTANT", "ChatGPT")
+DEFAULT_ASSISTANT = os.getenv("DEFAULT_ASSISTANT", "ChatGPT")
 
 # Manages assistants
 registry = Registry()
@@ -28,7 +28,7 @@ def get_chat_state(chat_id: str) -> ChatState:
     try:
         return ChatState.load_from_disk(chat_id)
     except FileNotFoundError:
-        return ChatState.create_new(chat_id, default_assistant)
+        return ChatState.create_new(chat_id, _get_default_assistant().name)
 
 
 def get_chat(chat_id: str) -> Chat:
@@ -42,5 +42,12 @@ def get_assistant(message: models.SendMessageRequest, chat: Chat = Depends(get_c
     if message.content.startswith("@"):
         assistant = message.content[1:].split()[0]
     if not assistant:
-        assistant = default_assistant
+        assistant = _get_default_assistant().name
     return registry.get_assistant(assistant)
+
+
+def _get_default_assistant() -> Assistant:
+    try:
+        return registry.get_assistant(DEFAULT_ASSISTANT)
+    except UnknownAssistant:
+        return registry.assistants[0]
